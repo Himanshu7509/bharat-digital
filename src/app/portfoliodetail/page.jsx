1c9e8360e6e65acdfbc2a5f71763b6d34d99cc85
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-06-03 13:17:07 +0530

    3

---
+++ src/app/portfoliodetail/page.jsx
@@ -0,0 +1,10 @@
+import PortfolioDetails from '@/components/pages/portfolioDetails/PortfolioDetails'
+import React from 'react'
+
+const PortfolioDetailPage = () => {
+  return (
+    <div><PortfolioDetails/></div>
+  )
+}
+
+export default PortfolioDetailPage
